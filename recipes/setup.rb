df0779bdb214c41307e64aec99af718261e9e564
mrtyson93 <mrtyson@umich.edu>
2017-06-17 21:38:03 +0000

    added ohai system attributes to setub for motd

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -12,8 +12,16 @@ end
 
 package 'ntp' 
 
+#node['ipaddress']
+#node['memory']['total']
+ 
 file '/etc/motd' do
-  content 'This server is the property of Mitchell Tyson'
+  content "This server is the property of Mitchell Tyson
+  HOSTNAME: #{node['hostname']}   
+  IPADDRESS: #{node['ipaddress']} 
+  CPU: #{node['cpu']['0']['mhz']}
+  MEMORY: #{node['memory']['total']}
+"
   action :create
   owner 'root'
   group 'root'
