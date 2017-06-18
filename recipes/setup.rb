ea58b3845e990afe0dce10320975764fb2091cae
mrtyson93 <mrtyson@umich.edu>
2017-06-18 01:06:13 +0000

    common resources section 8

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -21,6 +21,20 @@ template '/etc/motd' do
     :name => 'My name is Mitchell'
   )
   action :create
+  owner 'root'
+  group 'root'
+end
+
+user 'user1' do
+  comment 'user1'
+  uid '123'
+  home '/home/user1'
+  shell '/bin/bash'
+end
+
+group 'admins' do
+  members 'user1'
+  append true
 end
 
 
